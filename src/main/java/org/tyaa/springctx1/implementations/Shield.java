d817da7e6534b27abfde042a92a2fd07a763ca10
Renya <Renya@Renegad>
2018-08-25 09:49:47 +0300

    add protector and shield beans

---
+++ src/main/java/org/tyaa/springctx1/implementations/Shield.java
@@ -0,0 +1,11 @@
+package org.tyaa.springctx1.implementations;
+
+import org.tyaa.springctx1.interfaces.ITool;
+
+public class Shield implements ITool {
+	
+	public void doAction() {
+
+		System.out.println("Block damage from the enemy\n");
+	}
+}
